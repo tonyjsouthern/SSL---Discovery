88cd886f71bcf11fd87393224cfe9460c6d92ba9
TJ <tj.southern@salesfusion.com>
2019-04-05 08:38:12 -0700

    organized functions in occurring order

--- index.js
+++ index.js
@@ -1,88 +1,28 @@
-//var express         = require('express');
-var fs	          = require("fs");
-var sslChecker    = require('ssl-checker')
-var dns           = require('dns').promises;
-var Connection    = require('tedious').Connection;
-var sql           = require('sequelize');
+var fs 				 = require("fs");
+var sslChecker = require('ssl-checker')
+var dns				 = require('dns').promises;
+var Connection = require('tedious').Connection;
+var sql				 = require('sequelize');
 require('dotenv').config();
 
 var config = {
-	host: process.env.DB_HOST,
-	port: "1433",
-	dialect: "mssql",
-	userName: process.env.DB_USERNAME,
-	password: process.env.DB_PASSWORD,
-	database: process.env.DB_DATABASE
+    host: process.env.DB_HOST,
+    port: "1433",
+    dialect: "mssql",
+    userName: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
 };
 
 var query = process.env.DB_QUERY;
 var customerArray;
 
-async function getCustomerCnames() {
-	var connection = await new sql(config.database, config.userName, config.password, config);
-	await connection.query(query).spread((results) => {
-		customerArray = results;
-		console.log("Total customers to check: " + results.length);
-	})
-};
+init();
 
 async function init() {
-	await checkResults();
-	await getCustomerCnames();
-	await loopCustomers();
-}
-
-async function checkSSL(domain) {
-	return sslChecker(domain)
-		.then((response) => {
-			return response;
-		})
-		.catch((error) => {
-			console.log(error);
-		});
-}
-
-async function runDnsResolve(domain) {
-	try {
-		var result = await dns.resolveCname(domain);
-		if (result[0] == 'hostssl.msgapp.com' || result[0] == 'eu.hostssl.msgapp.com') {
-			return true;
-		} else {
-			return false;
-		}
-	} catch (error) {
-		return false;
-	}
-}
-
-function writeData(resultSet) {
-	return fs.appendFileSync("results.txt", resultSet, (err) => {
-		if (err) throw err;
-	});
-}
-
-function convertDate(d) {
-	var parts = d.split(" ");
-	if (parts[1] == '') {
-		return parts[0] + "-" + parts[2] + "-" + parts[4];
-	} else {
-		return parts[0] + "-" + parts[1] + "-" + parts[3];
-	}
-}
-
-async function loopCustomers() {
-	for (const customer of customerArray) {
-		try {
-			var resolved = await runDnsResolve(customer.hostedlink)
-			if (resolved == true) {
-				var results = await sslChecker(customer.hostedlink)
-				var stringData = customer.customername.replace(/,/g, '') + "," + customer.customer_id + "," + convertDate(results.valid_from) + "," + convertDate(results.valid_to) + "," + results.days_remaining + "\n";
-				await writeData(stringData)
-			}
-			console.log(await "Ran check on customer " + customer.customername + ": " + resolved + ".");
-		} catch (e) {
-		}
-	}
+    await checkResults();
+    await getCustomerCnames();
+    await loopCustomers();
 }
 
 function checkResults() {
@@ -93,8 +33,68 @@ function checkResults() {
             });
         } else {
             return
-        };
+        }
     });
 }
 
-init();
+async function getCustomerCnames() {
+    var connection = await new sql(config.database, config.userName, config.password, config);
+    await connection.query(query).spread((results) => {
+        customerArray = results;
+        console.log("Total customers to check: " + results.length);
+    })
+}
+
+async function loopCustomers() {
+    for (const customer of customerArray) {
+        try {
+            var resolved = await runDnsResolve(customer.hostedlink)
+            if (resolved == true) {
+                var results = await sslChecker(customer.hostedlink)
+                var stringData = customer.customername.replace(/,/g, '') + "," + customer.customer_id + "," + convertDate(results.valid_from) + "," + convertDate(results.valid_to) + "," + results.days_remaining + "\n";
+                await writeData(stringData)
+            }
+            console.log(await "Ran check on customer " + customer.customername + ": " + resolved + ".");
+        } catch (e) {
+					return
+				}
+    }
+}
+
+async function runDnsResolve(domain) {
+    try {
+        var result = await dns.resolveCname(domain);
+        if (result[0] == 'hostssl.msgapp.com' || result[0] == 'eu.hostssl.msgapp.com') {
+            return true;
+        } else {
+            return false;
+        }
+    } catch (error) {
+        return false;
+    }
+}
+
+async function checkSSL(domain) {
+    return sslChecker(domain)
+        .then((response) => {
+            return response;
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+}
+
+function convertDate(d) {
+    var parts = d.split(" ");
+    if (parts[1] == '') {
+        return parts[0] + "-" + parts[2] + "-" + parts[4];
+    } else {
+        return parts[0] + "-" + parts[1] + "-" + parts[3];
+    }
+}
+
+function writeData(resultSet) {
+    return fs.appendFileSync("results.txt", resultSet, (err) => {
+        if (err) throw err;
+    });
+}
